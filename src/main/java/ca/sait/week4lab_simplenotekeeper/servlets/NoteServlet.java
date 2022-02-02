072611a2152a3da776dc3145992dbfc139ca0b15
EcMangawang <emmari.mangawang@edu.sait.ca>
2022-02-01 19:39:47 -0700

    View Note complete
    The title shows the title of note.txt, the contents show the contents of note.txt

---
+++ src/main/java/ca/sait/week4lab_simplenotekeeper/servlets/NoteServlet.java
@@ -0,0 +1,56 @@
+package ca.sait.week4lab_simplenotekeeper.servlets;
+
+import ca.sait.week4lab_simplenotekeeper.servlets.javabens.Note;
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author emmari
+ */
+public class NoteServlet extends HttpServlet {
+
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String path = getServletContext().getRealPath("/WEB-INF/note.txt");
+        BufferedReader br = new BufferedReader(new FileReader(new File(path)));
+        
+        String title = br.readLine();
+        String contents = br.readLine();
+        
+        Note note = new Note (title, contents);
+        
+        request.setAttribute("note", note);
+        
+        getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp").forward(request,response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/viewnote.jsp
@@ -0,0 +1,22 @@
+<%-- 
+    Document   : viewnote
+    Created on : Feb. 1, 2022, 7:01:49 p.m.
+    Author     : emmar
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>View Note</title>
+    </head>
+    <body>
+        <h1>View Note</h1>
+        
+        <p>Title: ${note.title}</p>
+        <p>Contents: ${note.contents}</p>
+        
+        <a href="#">Edit Note</a>
+    </body>
+</html>
